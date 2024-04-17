66d088cafa027c218afad7de615651d55896b0ed
YJ <gg3582@naver.com>
2024-04-17 16:55:25 +0900

    DTO GraphQL 작성 완료 TestResolve 수정 완료

--- src/main/kotlin/com/daelim/springtest/main/resolver/TestResolve.kt
+++ src/main/kotlin/com/daelim/springtest/main/resolver/TestResolve.kt
@@ -3,12 +3,15 @@ package com.daelim.springtest.main.resolver
 import com.daelim.springtest.main.api.model.dto.TestDto
 import graphql.kickstart.tools.GraphQLMutationResolver
 import graphql.kickstart.tools.GraphQLQueryResolver
-import kotlin.random.Random
+import net.datafaker.Faker
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
-class TestResolver : GraphQLQueryResolver, GraphQLMutationResolver {
+class PostResolver : GraphQLQueryResolver, GraphQLMutationResolver {
     private val tests = mutableListOf<TestDto>()
+
+    val faker = Faker(Locale.KOREA)
 
     fun findAllTests(): List<TestDto> {
         return tests
@@ -18,21 +21,15 @@ class TestResolver : GraphQLQueryResolver, GraphQLMutationResolver {
         return tests.find { it.id == id }
     }
 
-    fun createTest(testDtoInput: TestDtoInput): TestDto {
-        val adjectives = arrayOf("용감한", "귀여운", "멋진", "친절한", "똑똑한")
-        val nouns = arrayOf("사자", "토끼", "여우", "너구리", "펭귄")
-
-        val randomNickname = "${adjectives[Random.nextInt(adjectives.size)]} ${nouns[Random.nextInt(nouns.size)]}"
-
-        val testDto = TestDto(
-            id = testDtoInput.id,
-            nickname = randomNickname
+    fun createTest(userId: String): TestDto {
+        val test = TestDto(
+            id = userId,
+            address = faker.address().fullAddress(),
+            email = faker.internet().emailAddress(),
+            tel = faker.phoneNumber().phoneNumber(),
+            age = Random().nextInt(100)
         )
-        tests.add(testDto)
-        return testDto
+        tests.add(test)
+        return test
     }
 }
-
-data class TestDtoInput(
-    val id: String
-)
